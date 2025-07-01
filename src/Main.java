23b66a52fa3d4bb8eb96f580d1420aebe5649c93
Adrian Michniewicz <adrian.michniewicz13@gmail.com>
2025-07-01 18:40:06 +0200

    Pierwszy commit: dodano prosty kalkulator w Javie

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Prosty kalkulator dodawania");
+        System.out.print("Podaj pierwszą liczbę: ");
+        int a = scanner.nextInt();
+
+        System.out.print("Podaj drugą liczbę: ");
+        int b = scanner.nextInt();
+
+        int suma = a + b;
+
+        System.out.println("Suma " + a + " + " + b + " = " + suma);
+        scanner.close();
+    }
+}
